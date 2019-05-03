2882a3a8f88ebafbb9fcc848b7357977b453f2b0
thannaree <bobojunga@gmail.com>
2019-05-02 22:13:35 -0500

    Committing all files from the original code

---
+++ bowSite.js
@@ -0,0 +1,20 @@
+/*
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {
+    myFunction()
+};
+
+// Get the header
+var topNav = document.getElementById("top-nav");
+
+// Get the offset position of the navbar
+var sticky = topNav.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        topNav.classList.add("sticky");
+    } else {
+        topNav.classList.remove("sticky");
+    }
+}*/
