65004d665218bd5deb4bdc7a03c298407827f1f8
thannaree <bobojunga@gmail.com>
2019-06-30 12:13:37 -0500

    adding more photos and mobile navigation

--- bowSite.js
+++ bowSite.js
@@ -1,3 +1,20 @@
+/* A stylesheet specifically the Media Query for to use on my website
+   !* Author: Thannaree Charoenpokkasap
+   !* Website: bow.meys.tel, thannaree.github.io, bow.meystel.com
+*/
+
+// A function for typing effect
+function mobileNav() {
+    let theLink = document.getElementById("side-nav");
+
+    if (theLink.style.display === "block") {
+        theLink.style.display = "none";
+    }
+    else {
+        theLink.style.display = "block";
+    }
+}
+
 /*
 // When the user scrolls the page, execute myFunction
 window.onscroll = function() {
@@ -10,7 +27,8 @@ var topNav = document.getElementById("top-nav");
 // Get the offset position of the navbar
 var sticky = topNav.offsetTop;
 
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// Add the sticky class to the header when you reach its scroll position.
+Remove "sticky" when you leave the scroll position
 function myFunction() {
     if (window.pageYOffset > sticky) {
         topNav.classList.add("sticky");
